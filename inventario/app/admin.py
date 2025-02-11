7f643665bbb9fca48072daa1df687b83dbab8fd2
Israel <is.delpino@duocuc.cl>
2025-02-11 16:03:30 -0300

    excel-terminado-por -sucursales

--- inventario/app/admin.py
+++ inventario/app/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Sucursal
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'sucursal')  # Muestra las columnas en la lista
+    list_filter = ('sucursal',)  # Agrega el filtro por sucursal
 
 admin.site.register(Producto)
+admin.site.register(Sucursal)
